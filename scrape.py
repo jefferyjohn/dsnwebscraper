1800d9ea8a5d0fa22574b8e40f9bd4ca974a2308
Dhruva Reddy <dhruva2000@gmail.com>
2024-01-14 17:44:07 -0500

    working script

--- scrape.py
+++ scrape.py
@@ -6,59 +6,71 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager 
 from selenium.webdriver.common.by import By 
  
+variables = ['NAME', 'RANGE', 'ROUND-TRIP LIGHT TIME', 'NAME', 'AZIMUTH', 'ELEVATION', 'WIND SPEED', 'MODE', 'SOURCE', 'FREQUENCY BAND', 'DATA RATE', 'POWER RECEIVED']
+
+res = dict.fromkeys(variables, 0)
+
+
 URL = "https://eyes.nasa.gov/dsn/dsn.html"
  
 driver = webdriver.Chrome(service=ChromeService( 
 	ChromeDriverManager().install())) 
- 
+
+driver.implicitly_wait(3)
+
 driver.get(URL) 
 time.sleep(3)
 
+
 small_dishes = driver.find_elements(By.CSS_SELECTOR, "div[class*='figure small_dish'")
 
-# print(driver.page_source)
-# for elem in content:
-#     test = driver.find_element(By.CSS_SELECTOR, "a[id*='spacecraft_0_2_0'")
-#     print(test.accessible_name)
-# # print(content)
-# print(len(small_dishes))
-
 for i in range(3):
+    # --------------------------------- first row -------------------------------- #
     if i == 0:
         for j in range(5):
-            temp = "spacecraft_" + str(i) + "_" + str(j+1)
+            temporary = "spacecraft_" + str(i) + "_" + str(j+1) #this should look like "spacecraft_0_1"
             try:
-                test = small_dishes[i+j].find_element(By.CSS_SELECTOR, f"a[id*={temp}]")
-                # if test:
-                print(test.accessible_name)
+                spacecraft_div = small_dishes[i+j].find_element(By.CSS_SELECTOR, f"div[id*={temporary}]")
+                actual_title = spacecraft_div.find_elements(By.CSS_SELECTOR, f"a[id*={temporary}]")
+
+                for item in range(len(actual_title)): #if there are multiple items in the list
+                    if actual_title[item].accessible_name == "APM1":
+                        if actual_title[item].get_attribute("class") != "selected":
+                            driver.find_element(By.CSS_SELECTOR, f"a[id*={temporary}_{item}]").click()
+                        driver.find_element(By.CSS_SELECTOR, f"a[title*='more detail']").click()
+                        sidebar = driver.find_element(By.CSS_SELECTOR, "div[id*='current_data']")
+                        values = sidebar.find_elements(By.TAG_NAME, 'p')
+                        for e in range(len(values)):
+                            res[variables[e]] = values[e].text
+                            # print(res[variables[e]])
+                            print(values[e].text)
+                        print(res)
+                        break
             except:
                 print("No element found")
+    # ----------------------------- for rows 2 and 3 ----------------------------- #
     else:
         for j in range(3):
-            temp = "spacecraft_" + str(i) + "_" + str(j+1)
+            temporary = "spacecraft_" + str(i) + "_" + str(j+1) #this should look like "spacecraft_0_1"
             try:
-                test = small_dishes[5+3*(i-1)+j].find_element(By.CSS_SELECTOR, f"a[id*={temp}]")
-                # if test:
-                print(test.accessible_name)
+                spacecraft_div = small_dishes[5+3*(i-1)+j].find_element(By.CSS_SELECTOR, f"div[id*={temporary}]")
+                actual_title = spacecraft_div.find_elements(By.CSS_SELECTOR, f"a[id*={temporary}]")
+
+                for item in range(len(actual_title)): #if there are multiple items in the list
+                    if actual_title[item].accessible_name == "APM1":
+                        if actual_title[item].get_attribute("class") != "selected":
+                            driver.find_element(By.CSS_SELECTOR, f"a[id*={temporary}_{item}]").click()
+                        driver.find_element(By.CSS_SELECTOR, f"a[title*='more detail']").click()
+                        sidebar = driver.find_element(By.CSS_SELECTOR, "div[id*='current_data']")
+                        values = sidebar.find_elements(By.TAG_NAME, 'p')
+                        for e in range(len(values)):
+                            res[variables[e]] = values[e].text
+                            # print(res[variables[e]])
+                            print(values[e].text)
+                        print(res)
+                        break
             except:
                 print("No element found")
 
 
-
-
-
-            # temp = "spacecraft_" + str(i) + "_" + str(j+1)
-            # try:
-            #     test = small_dishes[5+3*(i-1)+j].find_elements(By.CSS_SELECTOR, f"div[id*={temp}]")
-            #     print(len(test))
-            #     for i in range(len(test)):
-            #         print(f"a[id*={temp}_{i}]")
-            #         item = test.find_element(By.CSS_SELECTOR, f"a[id*={temp}_{i}]")
-            #         print(item.accessible_name)
-
-            #     # if test:
-                
-            # except:
-            #     print("No element found")
-            
 
